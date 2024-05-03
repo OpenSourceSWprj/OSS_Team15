7e71c858a8025b0c0c9379c58d8c55fb4b0f3789
9hon9 <164728750+9hon9@users.noreply.github.com>
2024-05-03 20:21:23 +0900

    gpt api
    gpt api를 라이브러리로 불러와서 기본적인 gpt 채팅기능 구현
    api_key는 환경변수로 설정
    환경변수는 windows 운영체제로 설정. 따로 api key 입력해줘야 함

---
+++ ChatBot.py
@@ -0,0 +1,38 @@
+from openai import OpenAI
+import openai
+import os
+
+client = OpenAI(api_key = os.getenv("OPENAI_API_KEY"))
+
+history_message = [
+     {"role": "system", "content": "You are the best resume consultant professor"}
+]
+
+end_word = '종료'
+print(f"자기소개서 질문을 입력해주세요. 질문이 끝났다면 \"{end_word}\" 라고 말해주세요.\n\n")
+user_question = ""
+
+while True:
+    user_question = input("Any question? ")
+
+    if user_question == end_word:
+        print(f"이용해주셔서 감사합니다. 합격을 기원합니다.")
+        break
+
+    history_message.append({
+        "role": "user",
+        "content": user_question})
+
+    completion = client.chat.completions.create(
+      model="gpt-3.5-turbo",
+      messages=history_message
+    )
+
+    response = completion.choices[0].message.content
+    print(response)
+
+    history_message.append({
+      "role": "assistant",
+      "content": response})
+
+    print("\n\n")
