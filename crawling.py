6c4be2fc0728c51b6a4e8aaa1fd7e6f8bba3da45
dongheeHan <ark182818@gmail.com>
2024-05-07 23:00:40 +0900

    text extract

--- crawling.py
+++ crawling.py
@@ -1,21 +1,18 @@
+import json
 import requests
 from bs4 import BeautifulSoup
 
-# 크롤링할 웹 페이지의 URL
-url = 'https://www.example.com'
+url = "https://linkareer.com/cover-letter/33171?page=1&sort=PASSED_AT&tab=all"
 
-# 웹 페이지에 GET 요청 보내기
+# 웹페이지에 요청을 보내고 응답을 받음
 response = requests.get(url)
+# 응답의 HTML 소스를 파싱하여 BeautifulSoup 객체 생성
+soup = BeautifulSoup(response.text, "html.parser")
+# class가 jss1340인 main 태그 확인
+main_tag = json.loads(soup.find("script", {"id": "__NEXT_DATA__"}).text)
+print(
+    main_tag["props"]["pageProps"]["data"]["coverLetterWithHighlight"]["coverLetter"]["content"]
+)
 
-# 요청이 성공했는지 확인
-if response.status_code == 200:
-    # HTML 파싱
-    soup = BeautifulSoup(response.text, 'html.parser')
-    
-    # 제목 추출
-    title = soup.title.text
-    
-    # 결과 출력
-    print("웹 페이지 제목:", title)
-else:
-    print("요청 실패:", response.status_code)
+# for page in range(1,643):
+#     	Hollys_url = 'https://linkareer.com/cover-letter/33171?page=%d&sort=PASSED_AT&tab=all' %page
