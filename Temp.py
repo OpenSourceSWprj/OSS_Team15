38ad701b599212c700992c3f5f3ce91e2eb666b3
StoneCAU <seokwoo1999@gmail.com>
2024-05-22 21:19:48 +0900

    change

--- Temp.py
+++ Temp.py
@@ -9,7 +9,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///C:\\Users\\seokw\\PycharmProj
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
-OPENAI_API_KEY = "sk-proj-VvldGEMD4RuhGImfVEM2T3BlbkFJ2GnK87eyYguabtOmjziE"
+OPENAI_API_KEY = "nope"
 client = OpenAI(api_key=OPENAI_API_KEY)
 # 데모에 필요한 최소한의 DB
 
